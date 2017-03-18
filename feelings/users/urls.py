915e602e81c74fa437cc2f604dd4e6068e3a0f67
Oleg Bloshenkin <obloshenkin@playtika.com>
2017-03-18 23:16:19 +0200

    Add logout options

--- feelings/users/urls.py
+++ feelings/users/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url('^', include('django.contrib.auth.urls')),
-    url(r'^dashboard/$', views.dashboard, name='dashboard')
-
 ]
