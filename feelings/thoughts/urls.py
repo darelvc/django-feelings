d44da18fe436a3412f513f26fd90e80b17864706
Oleg Bloshenkin <obloshenkin@playtika.com>
2017-03-19 01:01:43 +0200

    New commit for feelings project

---
+++ feelings/thoughts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^create/$', views.CreateThought.as_view(), name='create'),
+
+]
--- feelings/thoughts/views.py
+++ feelings/thoughts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .forms import ThoughtForm
 
-# Create your views here.
+
+class CreateThought(LoginRequiredMixin, CreateView):
+    form_class = ThoughtForm
+    success_url = reverse_lazy('users:dashboard')
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['user'] = self.request.user
+        return initial
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.get_form()
